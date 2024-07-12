4f96585a0dd362424aa0b87cee92c3e61aed355d
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-12 16:50:26 +0100

    JS lesson

---
+++ script.js
@@ -0,0 +1,43 @@
+// Variables
+// var, let, const
+var x;
+var y;
+
+x = 5;
+y = 6;
+let message = x +y;
+console.log(message)
+
+// Data types
+// Strings, numbers, boolean, array, objects
+
+let name = "Mr Mark";
+let number = 42;
+let isTrue = false;
+let fruits = ["apple", "banana", "cherry"];
+let person = { name: "John", age: 30 };
+
+console.log(person);
+
+
+// Functions
+
+function greet(name) {
+    return `Hello, ${name}!`;
+  }
+
+console.log(greet("Mr Essien"));
+
+
+
+// Control structures
+
+age = 2z;
+
+if (age > 18) {
+    console.log("Adult");
+  } else {
+    console.log("Minor");
+  }
+  
+  
